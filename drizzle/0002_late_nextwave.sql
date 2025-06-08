b97b0c156a2e0291f944d167666742eba1b37560
Michaela Hošková <66723714+misahoskova@users.noreply.github.com>
2025-06-08 16:37:43 +0200

    db

--- drizzle/0002_late_nextwave.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`username` text NOT NULL,
-	`hashedPassword` text NOT NULL,
-	`salt` text NOT NULL,
-	`token` text NOT NULL
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);
